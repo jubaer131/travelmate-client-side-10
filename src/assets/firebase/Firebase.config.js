203643bfd694ba8909f814bf4b97ec7d1de6a91f
juber131 <jubayerahmad131@gmail.com>
2024-04-28 14:18:26 +0600

    add all tour section

--- src/assets/firebase/Firebase.config.js
+++ src/assets/firebase/Firebase.config.js
@@ -5,23 +5,23 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyA4ByCdm9PEumTITaMdJ0pfJBMlqpKKx-k",
-  authDomain: "travel-mate-dfde0.firebaseapp.com",
-  projectId: "travel-mate-dfde0",
-  storageBucket: "travel-mate-dfde0.appspot.com",
-  messagingSenderId: "276130735368",
-  appId: "1:276130735368:web:bfc61024c092a565e0f8bd"
-};
-console.log(import.meta.env.VITE_authDomain)
 // const firebaseConfig = {
-//   apiKey:import.meta.env.VITE_apiKey,
-//   authDomain:import.meta.env.VITE_authDomain,
-//   projectId:import.meta.env.VITE_projectId,
-//   storageBucket:import.meta.env.VITE_storageBucket,
-//   messagingSenderId:import.meta.env.VITE_messagingSenderId,
-//   appId:import.meta.env.VITE_appId,
+//   apiKey: "AIzaSyA4ByCdm9PEumTITaMdJ0pfJBMlqpKKx-k",
+//   authDomain: "travel-mate-dfde0.firebaseapp.com",
+//   projectId: "travel-mate-dfde0",
+//   storageBucket: "travel-mate-dfde0.appspot.com",
+//   messagingSenderId: "276130735368",
+//   appId: "1:276130735368:web:bfc61024c092a565e0f8bd"
 // };
+
+const firebaseConfig = {
+  apiKey:import.meta.env.VITE_apiKey,
+  authDomain:import.meta.env.VITE_authDomain,
+  projectId:import.meta.env.VITE_projectId,
+  storageBucket:import.meta.env.VITE_storageBucket,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId,
+  appId:import.meta.env.VITE_appId,
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
