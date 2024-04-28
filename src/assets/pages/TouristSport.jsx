b0cf576bcc9dfc19041fb25250209cf6235fdbf7
juber131 <jubayerahmad131@gmail.com>
2024-04-29 00:03:31 +0600

    add footer

--- src/assets/pages/TouristSport.jsx
+++ src/assets/pages/TouristSport.jsx
@@ -16,10 +16,18 @@ const TouristSport = () => {
     }, [])
 
     return (
-        <section className="py-6 sm:py-12 dark:bg-gray-100 dark:text-gray-800 grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-8">
-            {
-                item.map(data => <TouristSport2 data={data}></TouristSport2>)
-            }
+        <section >
+
+            <h1 className="text-center text-4xl font-bold md:mt-20 mb-4">Popular tourist sport</h1>
+            <p className="text-center text-xl font-medium">Popular tourist spots vary greatly depending on the region and the interests of travelers.Find your destinations</p>
+            <div className="py-6 sm:py-12 dark:bg-gray-100 dark:text-gray-800 grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-8">
+                {
+                    item.map(data => <TouristSport2 data={data}></TouristSport2>)
+                }
+            </div>
+
+
+
 
         </section>
     );
--- src/assets/pages/TouristSport2.jsx
+++ src/assets/pages/TouristSport2.jsx
@@ -8,13 +8,14 @@ const TouristSport2 = ({ data }) => {
         traveltime, username, visitor, _id } = data
     return (
         <div className=" rounded-md shadow-md dark:bg-gray-50 dark:text-gray-800">
+
             <img src={Photo} alt="" className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500" />
             <div className="flex flex-col justify-between p-6 space-y-8">
                 <div className="space-y-2">
-                    <h2 className="text-3xl font-semibold tracking-wide"> {name}</h2>
-                    <p className="dark:text-gray-800"> {country}</p>
+                    <h2 className="text-2xl font-semibold tracking-wide"> {name}</h2>
+                    <p className="dark:text-gray-800 pb-2"> {country}</p>
                     <Link to={`/touristsport/${_id}`}>
-                        <button type="button" className="flex items-start justify-start w-28 p-3 font-semibold tracking-wide rounded-md dark:bg-violet-600 dark:text-gray-50 btn btn-primary">View details</button>
+                        <button type="button" className="flex items-start justify-start w-28 p-3 font-semibold tracking-wide rounded-md  text-white btn bg-cyan-600">View details</button>
                     </Link>
 
                 </div>
